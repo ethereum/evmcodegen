82d38bbec84ab5f9cbd8f2155fb7a87f10d1d62a
tintinweb <tintinweb@oststrom.com>
2018-10-10 21:52:30 +0200

    add setup.py

---
+++ evmcodegen/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+version = "0.1.0"
+name = "evmcodegen"
+
+setup(
+    name=name,
+    version=version,
+    packages=find_packages(),
+    author="tintinweb",
+    author_email="tintinweb@oststrom.com",
+    description=(
+        "--TBD--"),
+    license="GPLv2",
+    keywords=["evmdasm", "ethereum", "blockchain", "evm"],
+    url="https://github.com/tintinweb/%s"%name,
+    download_url="https://github.com/tintinweb/%s/tarball/v%s"%(name,version),
+    #python setup.py register -r https://testpypi.python.org/pypi
+    long_description=read("README.md") if os.path.isfile("README.md") else "",
+    long_description_content_type='text/markdown',
+    install_requires=[""],
+    #package_data={},
+    #extras_require={},
+)
